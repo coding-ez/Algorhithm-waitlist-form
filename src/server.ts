190494b3bb36acf5b7a91fd3ea52406acf572daa
Kalpesh Borde <bordekalpesh@gmail.com>
2025-07-30 19:10:18 +0530

    cors

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,12 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
+
 app.use(express.json());
 app.use("/api", waitlistRoute);
 
